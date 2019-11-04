e82a7edc1572729b787e6f4c2374341e0a6ea77a
Haim D <haim11293@aol.com>
2019-11-04 21:44:53 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -34,4 +34,16 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
+  def self.oldest_customer
+    oldest_age = 0
+    oldest_customer = nil
+    self.all.each do |customer|
+      if customer.age > oldest_age
+        oldest_age = customer.age
+        oldest_customer = customer
+      end
+    end
+    oldest_customer
+  end
+
 end
